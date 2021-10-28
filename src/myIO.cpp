55e110c62cedf675ba43c8319f39f1e4ca04893e
Marco <marcomarchetti8689@gmail.com>
2021-10-28 14:33:16 +0200

    aggiunti print seriali

--- src/myIO.cpp
+++ src/myIO.cpp
@@ -18,4 +18,20 @@ void write_command(t_cmd* command_ptr){
     Serial.print(command_ptr->command);
     Serial.print(" valore: ");
     Serial.println(command_ptr->value);
+    if (command_ptr->command == 'r'){
+        Serial.print("red LED power = ");
+        Serial.println(command_ptr->value);
+    }
+    else if(command_ptr->command == 'g'){
+        Serial.print("green LED power = ");
+        Serial.println(command_ptr->value);
+    }
+    else if(command_ptr->command == 'b'){
+        Serial.print("blue LED power = ");
+        Serial.println(command_ptr->value);
+    }
+    else{
+        Serial.println("invalid command");
+    }
+
 }
