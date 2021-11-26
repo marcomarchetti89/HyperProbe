70065317062ddffa7a6710b377b38afb6c0468c5
Marco <marcomarchetti8689@gmail.com>
2021-11-26 10:44:36 +0100

    definizioni nuovi led aggiunti e rimozione parte analogica su globals.cpp

--- src/globals.cpp
+++ src/globals.cpp
@@ -7,8 +7,6 @@
 t_cmd cmd_buffer;  //buffer per il comando
 t_led led_buffer;  //buffer per riempire info led
 uint8_t power_resolution = 12;  //risoluzione in bit della corrente
-uint8_t analog_power_resolution = 12;  //risoluzione in bit della lettura analogica
-bool analog_ctrl = false; 
 t_led leds[NUMERO_LED];
 int LedSettlingTime = 10;
 int exp_time = 10000;
@@ -16,13 +14,15 @@ int ReadoutTime = 1000;
 
 //PINS
 const uint8_t PIN_PWR_12V = 10;      //da connettere al rele
-const uint8_t PIN_RED_LED = 19;      //pwm led rosso     
-const uint8_t PIN_GREEN_LED = 14;    //pwm led verde
-const uint8_t PIN_BLUE_LED = 37;     //pwm led blu
+const uint8_t PIN_FRD_LED = 23;      //pwm led far led
+const uint8_t PIN_DRD_LED = 22;      //pwm led deep red
+const uint8_t PIN_DRD_LED = 19;      //pwm led red-orange
+const uint8_t PIN_AMB_LED = 18;      //pwm led amber
+const uint8_t PIN_GRN_LED = 15;      //pwm led green
+const uint8_t PIN_BLU_LED = 14;      //pwm led blue
+const uint8_t PIN_RYL_LED = 22;      //pwm led royal blue
 const uint8_t PIN_CAMERA = 4;        //to trigger the camera
 const uint8_t PIN_START = 23;        //to start acquisition
-const uint8_t ANALOG_POWER_RED_LED = 21;      //to set analog reference red led 
-const uint8_t ANALOG_POWER_GREEN_LED = 17;    //to set analog reference green led 
-const uint8_t ANALOG_POWER_BLUE_LED = 15;     //to set analog reference blue led 
 
-//
+
+
