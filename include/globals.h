15d47f07fed537ab98b9b9e0e3aa4a9bc2bd1b08
Marco <marcomarchetti8689@gmail.com>
2021-11-26 10:57:32 +0100

    aggiornamento led su globals.h

--- include/globals.h
+++ include/globals.h
@@ -7,10 +7,23 @@
 
 /*lo definisco costante perché poi crerò un array di led
 e se è variabile devo mallocare la memoria*/
-#define NUMERO_LED 3 
-#define RED_INDEX 0
-#define GREEN_INDEX 1
-#define BLUE_INDEX 2
+#define NUMERO_LED 7
+#define FRD_INDEX 0
+#define DRD_INDEX 1
+#define RNG_INDEX 2
+#define AMB_INDEX 3
+#define GRN_INDEX 4
+#define BLU_INDEX 5
+#define RYL_INDEX 6
+
+extern const uint8_t PIN_FRD_LED;      //pwm led far led
+extern const uint8_t PIN_DRD_LED;      //pwm led deep red
+extern const uint8_t PIN_RNG_LED;      //pwm led red-orange
+extern const uint8_t PIN_AMB_LED;      //pwm led amber
+extern const uint8_t PIN_GRN_LED;      //pwm led green
+extern const uint8_t PIN_BLU_LED;      //pwm led blue
+extern const uint8_t PIN_RYL_LED;      //pwm led royal blue
+
 
 //definizione del tipo comando = lettera + intero
 typedef struct {
@@ -32,8 +45,6 @@ typedef struct {
 //variabili globali
 extern t_cmd cmd_buffer;  //buffer per il comando
 extern uint8_t power_resolution;  //risoluzione in bit della corrente
-extern uint8_t analog_power_resolution;  //risoluzione in bit della corrente settata analogicamente
-extern bool analog_ctrl; //controllo del setting analogico/digitale
 extern t_led leds[]; //vettore dei led
 extern int LedSettlingTime;
 extern int exp_time;
@@ -41,13 +52,14 @@ extern int ReadoutTime;
 
 //PINS
 extern const uint8_t PIN_PWR_12V;   //da connettere al rele da montere al posto dell'interruttore
-extern const uint8_t PIN_RED_LED;   //pin pwm led rosso       
-extern const uint8_t PIN_GREEN_LED; //pin pwm led verde       
-extern const uint8_t PIN_BLUE_LED;  //pin pwm led blu
+extern const uint8_t PIN_FRD_LED;      //pwm led far led
+extern const uint8_t PIN_DRD_LED;      //pwm led deep red
+extern const uint8_t PIN_RNG_LED;      //pwm led red-orange
+extern const uint8_t PIN_AMB_LED;      //pwm led amber
+extern const uint8_t PIN_GRN_LED;      //pwm led green
+extern const uint8_t PIN_BLU_LED;      //pwm led blue
+extern const uint8_t PIN_RYL_LED;      //pwm led royal blue
 extern const uint8_t PIN_CAMERA;    //pin trigger camera, al momento libero      
 extern const uint8_t PIN_START;     //pin interruttore fisico a filo
-extern const uint8_t ANALOG_POWER_RED_LED;   //AI su partitore relativo a led rosso 
-extern const uint8_t ANALOG_POWER_GREEN_LED; //AI su partitore relativo a led verde
-extern const uint8_t ANALOG_POWER_BLUE_LED;  //AI su partitore relativo a led blu
 
 #endif
